b2c4f7daab5fd9b5294ecebd50017ae3aef7b692
Danny Thibaudeau <cobreti_@hotmail.com>
2015-05-02 14:42:13 -0400

    added iOS version of the project

---
+++ projects/iOS/ProjectFV/DiagramElements/DiagramElements.h
@@ -0,0 +1,19 @@
+//
+//  DiagramElements.h
+//  DiagramElements
+//
+//  Created by Danny Thibaudeau on 2015-05-02.
+//  Copyright (c) 2015 Danny Thibaudeau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DiagramElements.
+FOUNDATION_EXPORT double DiagramElementsVersionNumber;
+
+//! Project version string for DiagramElements.
+FOUNDATION_EXPORT const unsigned char DiagramElementsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiagramElements/PublicHeader.h>
+
+
---
+++ projects/iOS/ProjectFV/VpProject/VpProject.h
@@ -0,0 +1,19 @@
+//
+//  VpProject.h
+//  VpProject
+//
+//  Created by Danny Thibaudeau on 2015-05-02.
+//  Copyright (c) 2015 Danny Thibaudeau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VpProject.
+FOUNDATION_EXPORT double VpProjectVersionNumber;
+
+//! Project version string for VpProject.
+FOUNDATION_EXPORT const unsigned char VpProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VpProject/PublicHeader.h>
+
+
