fdf57a3ee47fe47e6302a6af7a4efe1828cef27b
Danny Thibaudeau <cobreti_@hotmail.com>
2015-05-15 18:21:22 -0400

    updated iOS project

---
+++ projects/iOS/ProjectFV/Shapes/Shapes.h
@@ -0,0 +1,19 @@
+//
+//  Shapes.h
+//  Shapes
+//
+//  Created by Danny Thibaudeau on 2015-05-15.
+//  Copyright (c) 2015 Danny Thibaudeau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shapes.
+FOUNDATION_EXPORT double ShapesVersionNumber;
+
+//! Project version string for Shapes.
+FOUNDATION_EXPORT const unsigned char ShapesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shapes/PublicHeader.h>
+
+
