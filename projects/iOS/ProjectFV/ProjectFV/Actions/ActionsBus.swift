f65a00e6ff147fc1fa0be22a49b76ff2e6437853
Danny Thibaudeau <cobreti_@hotmail.com>
2015-07-12 19:43:49 -0400

    added writing of actions log to disk

--- projects/iOS/ProjectFV/ProjectFV/Actions/ActionsBus.swift
+++ projects/iOS/ProjectFV/ProjectFV/Actions/ActionsBus.swift
@@ -7,8 +7,9 @@
 //
 
 import Foundation
+import UIKit
 
-class ActionsBus {
+class ActionsBus : NSObject {
     
     var listeners : ActionListeners {
         get {
@@ -16,16 +17,43 @@ class ActionsBus {
         }
     }
     
-    init() {
+    override init() {
         _listeners = ActionListeners()
+        
+        super.init()
     }
     
     func send( action: Action ) {
         
         debugPrintln(action.description)
+        writeToLog(action.description)
         _listeners.send(action)
     }
     
+    func writeToLog(text: String) {
+        
+        let file = "actions_log_\(_timestamp).txt"
+        let textLine = "\(text)\n"
+        let data = textLine.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
+
+        
+        if  let dirs : [String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true) as? [String] {
+            let dir = dirs[0] //documents directory
+            let path = dir.stringByAppendingPathComponent(file);
+            let fileURL = NSURL(fileURLWithPath: path)
+            
+            if let  fileHandle = NSFileHandle(forWritingToURL: fileURL!, error: nil) {
+                    fileHandle.seekToEndOfFile()
+                    fileHandle.writeData(data)
+                    fileHandle.closeFile()
+            }
+            else {
+                textLine.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding, error: nil);
+            }
+
+        }
+    }
     
     private var _listeners : ActionListeners
+    private let _timestamp : NSDate = NSDate()
 }
